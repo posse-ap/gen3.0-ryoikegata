14fc6614fe92c595b12c63bab90a75d9d62883af
Ryo ikegata <ryo.ike.6153@gmail.com>
2022-10-28 11:29:26 +0900

    webapp　作成開始

---
+++ script/modal.js
@@ -0,0 +1,31 @@
+'use strict'
+
+{
+  const button = document.getElementById('modalButton');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+  const closeButton = document.getElementById('closeButton');
+
+
+  button.addEventListener('click', ()=>{
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  })
+
+  closeButton.addEventListener('click', () =>{
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  })
+
+
+
+
+
+
+
+  const loadingButton = document.getElementById('loadingButton');
+
+
+  loadingButton.addEventListener('click',() =>{　
+  })
+}
