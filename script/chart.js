6c38c5fbc829b29a7b146d2e6ba483a12eb62123
Ryo ikegata <ryo.ike.6153@gmail.com>
2022-11-02 11:18:00 +0900

    カレンダー作成

--- script/chart.js
+++ script/chart.js
@@ -1,110 +1,175 @@
-'use strict';
+"use strict";
 
 //bar
 {
-      (function (){
+  (function () {
+    let type = "bar";
 
-      let type = 'bar';
-
-      let data = {
-        labels: ['','2','','4','','6','','8','','10','','12','','14','','16','','18','','20','','22','','24','','26','','28','','30','',],
-        datasets: [{
-          data: [3, 3, 4, 4, 8, 3, 5, 5, 3, 7, 10, 2, 6, 5, 2, 1, 3, 5, 6, 4, 3, 7, 1, 11, 2, 4, 4, 1, 2, 3, 4,],
-          backgroundColor: 'skyblue',
+    let data = {
+      labels: [
+        "",
+        "2",
+        "",
+        "4",
+        "",
+        "6",
+        "",
+        "8",
+        "",
+        "10",
+        "",
+        "12",
+        "",
+        "14",
+        "",
+        "16",
+        "",
+        "18",
+        "",
+        "20",
+        "",
+        "22",
+        "",
+        "24",
+        "",
+        "26",
+        "",
+        "28",
+        "",
+        "30",
+        "",
+      ],
+      datasets: [
+        {
+          data: [
+            3, 3, 4, 4, 8, 3, 5, 5, 3, 7, 10, 2, 6, 5, 2, 1, 3, 5, 6, 4, 3, 7,
+            1, 11, 2, 4, 4, 1, 2, 3, 4,
+          ],
+          backgroundColor: "blue",
           borderWidth: 0,
-        }]
-      };
+        },
+      ],
+    };
 
-      let options = {
-        scales: {
-          xAxes: [{
+    let options = {
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
             stacked: true,
-            gridLines:{
-              drawBorder:false,
-              display:false
+            gridLines: {
+              drawBorder: false,
+              display: false,
             },
-          }],
-          yAxes: [{
+          },
+        ],
+        yAxes: [
+          {
             stacked: true,
-            gridLines:{
-              drawBorder:false,
-              display:false
+            gridLines: {
+              drawBorder: false,
+              display: false,
             },
-            ticks:{
-              min:0,
-              max:12
-            }
-          }]
-        }
-      };
+            ticks: {
+              min: 0,
+              max: 12,
+            },
+          },
+        ],
+      },
+    };
 
-      let ctx = document.getElementById('chart__bar').getContext('2d');
-      let myChart = new Chart(ctx, {
-        type: type,
-        data: data,
-        options: options
-      });
-    })();
+    let ctx = document.getElementById("chart__bar").getContext("2d");
+    let myChart = new Chart(ctx, {
+      type: type,
+      data: data,
+      options: options,
+    });
+  })();
+}
 
-  }
+//bar end
 
-  //bar end
+//lang
 
+{
+  (function () {
+    let type = "doughnut";
 
-  //lang
+    let data = {
+      labels: [
+        "Javascript",
+        "CSS",
+        "PHP",
+        "HTML",
+        "Larabel",
+        "SQL",
+        "SHELL",
+        "情報システム基礎知識（その他）",
+      ],
+      datasets: [
+        {
+          data: [48, 18, 10, 8, 6, 5, 4, 3],
+          backgroundColor: [
+            "rgb(4,69,236)",
+            "rgb(15,112,189)",
+            "rgb(32,189,222)",
+            "rgb(60,206,254)",
+            "rgb(178,158,243)",
+            "rgb(108,70,235)",
+            "rgb(74,23,239)",
+            "rgb(48,5,192)",
+          ],
+        },
+      ],
+    };
 
-  {
+    let options = {
+      cutoutPercentage: 40,
+    };
 
+    let ctx = document.getElementById("chart__lang").getContext("2d");
+    let myChart = new Chart(ctx, {
+      type: type,
+      data: data,
+      options: options,
+    });
+  })();
+}
 
-    (function() {
-      let type = 'doughnut';
+//lang end
 
-      let data = {
-        labels: ['Javascript', 'CSS', 'PHP','HTML','Larabel','SQL','SHELL','情報システム基礎知識（その他）',],
-        datasets: [{
-          data: [48, 18, 10, 8, 6, 5, 4,3,],
-          backgroundColor: ['tomato', 'skyblue', 'gray']
-        }]
-      };
+//content
 
-      let options = {
-        cutoutPercentage: 40
-      };
+{
+  (function () {
+    let type = "doughnut";
 
-      let ctx = document.getElementById('chart__lang').getContext('2d');
-      let myChart = new Chart(ctx, {
-        type: type,
-        data: data,
-        options: options
-      });
-    })();
-  }
+    let data = {
+      labels: ["ドットインストール", "N予備校", "POSSE課題"],
+      datasets: [
+        {
+          data: [42, 33, 25],
+          backgroundColor: [
+            "rgb(4,69,236)",
+            "rgb(15,112,189)",
+            "rgb(32,189,222)",
+          ],
+        },
+      ],
+    };
 
-  //lang end
+    let options = {
+      cutoutPercentage: 40,
+    };
 
-  //content
-
-  {
-    (function(){
-      let type = 'doughnut';
-
-      let data = {
-        labels:['','',''],
-        datasets:[{
-          data:[42,33,25],
-          backgroundColor:['skyblue','blue','green']
-        }]
-      };
-
-      let options = {
-        cutoutPercentage:40
-      }
-
-      let ctx = document.getElementById('chart__content').getContext('2d');
-      let myChart = new Chart(ctx, {
-        type:type,
-        data:data,
-        options:options
-      })
-    })();
-  }
+    let ctx = document.getElementById("chart__content").getContext("2d");
+    let myChart = new Chart(ctx, {
+      type: type,
+      data: data,
+      options: options,
+    });
+  })();
+}
--- script/modal.js
+++ script/modal.js
@@ -1,31 +1,172 @@
-'use strict'
+"use strict";
 
 {
-  const button = document.getElementById('modalButton');
-  const modal = document.getElementById('modal');
-  const mask = document.getElementById('mask');
-  const closeButton = document.getElementById('closeButton');
+  const button = document.getElementById("modalButton");
+  const modal = document.getElementById("modal");
+  const mask = document.getElementById("mask");
+  const closeButton = document.getElementById("closeButton");
 
+  button.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    mask.classList.remove("hidden");
+  });
 
-  button.addEventListener('click', ()=>{
-    modal.classList.remove('hidden');
-    mask.classList.remove('hidden');
-  })
+  closeButton.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+  });
 
-  closeButton.addEventListener('click', () =>{
-    modal.classList.add('hidden');
-    mask.classList.add('hidden');
-  })
+  //カレンダー
+  const data = document.getElementById("data");
+  const calendar = document.getElementById("calendar");
 
+  data.addEventListener("click", () => {
+    closeButton.classList.add("hidden");
+    loadingButton.classList.add("hidden");
+    modalContent.classList.add("hidden");
+    calendar.classList.remove("hidden");
+  });
 
+  //カレンダー機能
 
+  {
+    const today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth();
 
+    function getCalendarBody() {
+      const dates = [];
+      const lastDate = new Date(year, month + 1, 0).getDate();
 
+      for (let i = 1; i <= lastDate; i++) {
+        dates.push({
+          date: i,
+          isDisabled: false,
+        });
+      }
 
+      return dates;
+    }
 
-  const loadingButton = document.getElementById('loadingButton');
+    function getCalendarHead() {
+      const dates = [];
+      const d = new Date(year, month, 0).getDate();
+      const n = new Date(year, month, 1).getDay();
 
+      for (let i = 0; i < n; i++) {
+        dates.unshift({
+          date: d - i,
+          isDisabled: true,
+        });
+      }
 
-  loadingButton.addEventListener('click',() =>{
-  })
+      return dates;
+    }
+
+    function getCalendarTail() {
+      const dates = [];
+      const lastDay = new Date(year, month + 1, 0).getDay();
+
+      for (let i = 1; i < 7 - lastDay; i++) {
+        dates.push({
+          date: i,
+          isDisabled: true,
+        });
+      }
+
+      return dates;
+    }
+
+    function clearCalendar() {
+      const tbody = document.querySelector("tbody");
+
+      while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+      }
+    }
+
+    function titleCalendar() {
+      const title = `${year}/${month + 1}`;
+      document.getElementById("title").textContent = title;
+    }
+
+    function weekCalendar() {
+      const dates = [
+        ...getCalendarHead(),
+        ...getCalendarBody(),
+        ...getCalendarTail(),
+      ];
+
+      const weeks = [];
+      const weeksCount = dates.length / 7;
+
+      for (let i = 0; i < weeksCount; i++) {
+        weeks.push(dates.splice(0, 7));
+      }
+
+      weeks.forEach((week) => {
+        const tr = document.createElement("tr");
+        week.forEach((date) => {
+          const td = document.createElement("td");
+
+          td.textContent = date.date;
+          if (date.isDisabled) {
+            td.classList.add("disabled");
+          }
+
+          tr.appendChild(td);
+        });
+
+        document.querySelector("tbody").appendChild(tr);
+      });
+    }
+
+    function createCalendar() {
+      clearCalendar();
+      titleCalendar();
+      weekCalendar();
+    }
+
+    document.getElementById("prev").addEventListener("click", () => {
+      month--;
+      if (month < 0) {
+        year--;
+        month = 11;
+      }
+
+      createCalendar();
+    });
+    document.getElementById("next").addEventListener("click", () => {
+      month++;
+      if (month > 11) {
+        year++;
+        month = 0;
+      }
+
+      createCalendar();
+    });
+
+    createCalendar();
+  }
+
+  //loading
+  function loading() {
+    const loadingButton = document.getElementById("loadingButton");
+    const loading = document.getElementById("loading");
+    const modalContent = document.getElementById("modalContent");
+
+    loadingButton.addEventListener("click", () => {
+      loading.classList.remove("hidden");
+      closeButton.classList.add("hidden");
+      loadingButton.classList.add("hidden");
+      modalContent.classList.add("hidden");
+
+      if (loading.classList.remove("hidden")) {
+        setTimeout(function () {
+          loading.classList.add("hidden");
+        }, 3000);
+      }
+    });
+  }
+  loading();
 }
